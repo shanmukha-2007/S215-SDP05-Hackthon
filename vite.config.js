33eafe591f3bf0723e0936571aed5ad3dcfea0dd
shanmukha-2007 <shanmukhakrishna2007@gmail.com>
2026-02-09 09:37:22 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/S215-SDP05-Hackthon/',
-  plugins: [react()],
-})
