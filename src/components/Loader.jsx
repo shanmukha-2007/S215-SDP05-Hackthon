30c5b8c8315be8bff097442d6dc023897621e186
shanmukha-2007 <shanmukhakrishna2007@gmail.com>
2026-02-09 09:34:51 +0530

    Delete src directory

--- src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="flex items-center justify-center py-10">
-      <div className="animate-spin h-10 w-10 border-4 border-indigo-600 border-t-transparent rounded-full"></div>
-    </div>
-  );
-}
-if (loading) return <Loader />;
